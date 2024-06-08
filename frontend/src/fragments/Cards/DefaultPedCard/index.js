26575f348dfa140de90a84ef9ac26636f51f3ffc
DragosPatru <dragospatru93@gmail.com>
2024-06-08 20:07:55 +0300

    fix: reduce line-height for PED card

--- frontend/src/fragments/Cards/DefaultPedCard/index.js
+++ frontend/src/fragments/Cards/DefaultPedCard/index.js
@@ -41,7 +41,7 @@ function DefaultPedCard({ color, icon, title, description, action, deleteAction 
 					</MDTypography>
 				</MDBox>
 			</MDBox>
-			<MDBox pb={2} px={2} pt={1} textAlign="justify" lineHeight={1.25}>
+			<MDBox pb={2} px={2} pt={1} textAlign="justify" lineHeight={1}>
 
 				{description && (
 					<MDTypography variant="caption" color="text" fontWeight="regular">
